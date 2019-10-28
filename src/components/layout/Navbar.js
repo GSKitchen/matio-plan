c0b423e8a3b82ed55f3c31a9df017de119d64999
Sabbir Ahmad <sabbir.sio@gmail.com>
2019-10-28 18:37:19 +0530

    Added Sign up functionality

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,9 +6,13 @@ import SignedInLinks from "./SignedinLinks";
 import SignedOutLinks from "./SignedOutLinks";
 
 const Navbar = props => {
-  const { auth } = props;
+  const { auth, profile } = props;
   //console.log(auth);
-  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+  const links = auth.uid ? (
+    <SignedInLinks profile={profile} />
+  ) : (
+    <SignedOutLinks />
+  );
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
@@ -24,7 +28,8 @@ const Navbar = props => {
 const mapStateToProps = state => {
   console.log(state);
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
   };
 };
 
